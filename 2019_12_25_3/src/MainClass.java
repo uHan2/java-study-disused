9ec2027bfabd5cbe177e1f92d95d44c81442969b
uHan2 <asgd56@gmail.com>
2019-12-25 17:39:45 +0900

    Class, Object

---
+++ 2019_12_25_3/src/MainClass.java
@@ -0,0 +1,14 @@
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        TestClass test1 = new TestClass();
+        TestClass test2 = new TestClass();
+        TestClass test3 = new TestClass();
+
+        System.out.println("test1 : " + test1);
+        System.out.println("test2 : " + test2);
+        System.out.println("test3 : " + test3);
+
+    }
+}
