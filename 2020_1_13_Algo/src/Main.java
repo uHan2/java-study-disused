2c0b0a3c2e6c2072cd43ab532f28f46349abb8dd
uHan2 <asgd56@gmail.com>
2020-01-13 15:26:04 +0900

    BJ Algo BufferedReader & Writer

---
+++ 2020_1_13_Algo/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+        int a, b = 0;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a + b) + "\n");
+        }
+
+        bw.flush();
+    }
+}
