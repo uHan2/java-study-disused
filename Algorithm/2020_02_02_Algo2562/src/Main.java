42a111a888fc3e0c247f7d631ab12e482d1f2c9e
uHan2 <asgd56@gmail.com>
2020-02-03 22:48:15 +0900

    BJ Algo 2562

---
+++ Algorithm/2020_02_02_Algo2562/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[9];
+
+        for(int i = 0 ; i < arr.length ; i ++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        int max = arr[0];
+        int index = 1 ;
+
+        for(int i = 1 ; i < arr.length ; i ++)
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];
+                index = i+1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(index);
+
+    }
+}
