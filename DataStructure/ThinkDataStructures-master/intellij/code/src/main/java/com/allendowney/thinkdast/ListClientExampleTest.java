9ecca3313ff13f5642963dbfb03ad98ce4dd832c
uHan2 <asgd56@gmail.com>
2020-03-24 20:17:40 +0900

    DataStructuresTraining

--- DataStructure/ThinkDataStructures-master/intellij/code/src/main/java/com/allendowney/thinkdast/ListClientExampleTest.java
+++ DataStructure/ThinkDataStructures-master/intellij/code/src/main/java/com/allendowney/thinkdast/ListClientExampleTest.java
@@ -1,28 +1,29 @@
 package com.allendowney.thinkdast;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author downey
- *
  */
-public class ListClientExampleTest {
+public class ListClientExampleTest
+{
 
-	/**
-	 * Test method for {@link ListClientExample}.
-	 */
-	@Test
-	public void testListClientExample() {
-		ListClientExample lce = new ListClientExample();
-		@SuppressWarnings("rawtypes")
-		List list = lce.getList();
-		assertThat(list, instanceOf(ArrayList.class) );
-	}
+    /**
+     * Test method for {@link ListClientExample}.
+     */
+    @Test
+    public void testListClientExample()
+    {
+        ListClientExample lce = new ListClientExample();
+        @SuppressWarnings("rawtypes")
+        List list = lce.getList();
+        assertThat(list, instanceOf(ArrayList.class));
+    }
 
 }
