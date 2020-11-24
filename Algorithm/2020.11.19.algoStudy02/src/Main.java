72ecd2db813434136ee51ecc2afa7151c6dc7d05
uHan <asgd56@gmail.com>
2020-11-24 18:11:16 +0900

    Algorithm Study implement

--- Algorithm/2020.11.19.algoStudy02/src/Main.java
+++ Algorithm/2020.11.19.algoStudy02/src/Main.java
@@ -10,23 +10,25 @@ public class Main
         String nightPos = br.readLine();
 
         int row = nightPos.charAt(1) - '0';
-        int column = nightPos.charAt(0) - 'a' + 1;
+        int col = nightPos.charAt(0) - 'a' + 1;
 
         int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
         int[] dy = {-1, -2, -2, -1, 1, 2, 2, 1};
 
-        int result = 0;
+        int count = 0;
+
         for (int i = 0; i < 8; i++)
         {
             int nextRow = row + dx[i];
-            int nextColumn = column + dy[i];
-            if (nextRow >= 1 && nextRow <= 8 && nextColumn >= 1 && nextColumn <= 8)
+            int nextColumn = col + dy[i];
+
+            if ((1 <=nextRow && nextRow <= 8) &&  (1 <= nextColumn && nextColumn <= 8))
             {
-                result += 1;
+                count += 1;
             }
         }
 
-        System.out.println(result);
+        System.out.println(count);
     }
 
 }
