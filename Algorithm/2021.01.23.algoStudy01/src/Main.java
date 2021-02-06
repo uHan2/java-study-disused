5ca62b0de66999bc042063271c833d4cf2368fc0
uHan <asgd56@gmail.com>
2021-02-06 10:34:44 +0900

    AlgoStudy part03 chapter12 Implementation Problem Q.09

---
+++ Algorithm/2021.01.23.algoStudy01/src/Main.java
@@ -0,0 +1,90 @@
+import java.math.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String s = "aabbaccc";
+
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s)
+    {
+        int answer = s.length();
+
+        for (int step = 1; step <= s.length() / 2; step++)
+        {
+            String compressed = "";
+            String prev = s.substring(0, step);
+            int cnt = 1;
+
+            for (int j = step; j < s.length(); j += step)
+            {
+                String compare = "";
+
+                for (int k = j; k < j + step; k++)
+                {
+                    if (k < s.length()) compare += s.charAt(k);
+                }
+
+                if (prev.equals(compare)) cnt += 1;
+                else
+                {
+                    compressed += (cnt >= 2) ? cnt + prev : prev;
+                    prev = compare;
+                    cnt = 1;
+                }
+            }
+
+            compressed += (cnt >= 2) ? cnt + prev : prev;
+            answer = Math.min(answer, compressed.length());
+
+
+        }
+
+        return answer;
+    }
+
+}
+
+// import java.util.*;
+
+// class Solution {
+
+//     public int solution(String s) {
+//         int answer = s.length();
+//         // 1개 단위(step)부터 압축 단위를 늘려가며 확인
+//         for (int step = 1; step < s.length() / 2 + 1; step++) {
+//             String compressed = "";
+//             String prev = s.substring(0, step); // 앞에서부터 step만큼의 문자열 추출
+//             int cnt = 1;
+//             // 단위(step) 크기만큼 증가시키며 이전 문자열과 비교
+//             for (int j = step; j < s.length(); j += step) {
+//                 // 이전 상태와 동일하다면 압축 횟수(count) 증가
+//                 String sub = "";
+//                 for (int k = j; k < j + step; k++) {
+//                     if (k < s.length()) sub += s.charAt(k);
+//                 }
+//                 if (prev.equals(sub)) cnt += 1;
+//                 // 다른 문자열이 나왔다면(더 이상 압축하지 못하는 경우라면)
+//                 else {
+//                     compressed += (cnt >= 2)? cnt + prev : prev;
+//                     sub = "";
+//                     for (int k = j; k < j + step; k++) {
+//                         if (k < s.length()) sub += s.charAt(k);
+//                     }
+//                     prev = sub; // 다시 상태 초기화
+//                     cnt = 1;
+//                 }
+//             }
+//             // 남아있는 문자열에 대해서 처리
+//             compressed += (cnt >= 2)? cnt + prev : prev;
+//             System.out.println(compressed + " ");
+//             // 만들어지는 압축 문자열이 가장 짧은 것이 정답
+//             answer = Math.min(answer, compressed.length());
+//         }
+//         return answer;
+//     }
+// }
