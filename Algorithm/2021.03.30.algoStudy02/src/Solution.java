28b883253f6a8f3a3ad7a72a08956ab48c2dae77
uhan <asgd56@gmail.com>
2021-03-31 11:09:12 +0900

    AlgoStudy programmers level.2

---
+++ Algorithm/2021.03.30.algoStudy02/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public long solution(int w, int h)
+    {
+        long answer = 0;
+
+
+        for (int i = 1; i < w; i++)
+        {
+            answer += ((long) h * i) / w;
+        }
+
+        return answer * 2;
+    }
+}
